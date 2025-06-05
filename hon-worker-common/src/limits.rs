2e729e40aaefc349a8f00f325e7609e4c98f4374
rupansh <rupanshsekar@hotmail.com>
2025-06-06 00:11:10 +0530

    feat(hon-worker-common): add type for v2 balance endpoint

--- hon-worker-common/src/limits.rs
+++ hon-worker-common/src/limits.rs
@@ -1,4 +1,3 @@
-
 // Withdraw limits
 pub const MAX_WITHDRAWAL_PER_TXN: u64 = 2000;
 pub const MIN_WITHDRAWAL_PER_TXN: u64 = 1500;
@@ -6,6 +5,6 @@ pub const MAX_WITHDRAWAL_PER_DAY: u64 = 10000;
 
 // Reward limit
 pub const NEW_USER_SIGNUP_REWARD: u64 = 1000;
-pub const REFERRAL_REWARD: u64 = 100; 
+pub const REFERRAL_REWARD: u64 = 100;
 pub const AIRDROP_REWARD: u64 = 1000;
 pub const AIRDROP_REWARD_PER_DAY: u64 = 10000;
