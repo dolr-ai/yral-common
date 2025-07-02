543026f635ca75275b22f8acec045d7419431e23
komal-sai-yral <komalsai@gobazzinga.io>
2025-07-02 12:14:52 +0530

    ml feed cache

--- types/src/post.rs
+++ types/src/post.rs
@@ -1,5 +1,8 @@
 use ic_agent::export::Principal;
+use redis_macros::{FromRedisValue, ToRedisArgs};
 use serde::{Deserialize, Serialize};
+use std::hash::{Hash, Hasher};
+use utoipa::ToSchema;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct PostItem {
@@ -19,4 +22,41 @@ pub struct FeedRequest {
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct FeedResponse {
     pub posts: Vec<PostItem>,
+}
+
+#[derive(Serialize, Deserialize, Clone, ToSchema, Debug)]
+pub struct FeedRequestV2 {
+    pub user_id: String,
+    pub canister_id: String,
+    pub filter_results: Vec<String>, // List of video IDs to filter results
+    pub num_results: u32,
+}
+
+#[derive(Serialize, Deserialize, Clone, ToSchema, Debug, ToRedisArgs, FromRedisValue)]
+pub struct PostItemV2 {
+    pub publisher_user_id: String,
+    pub canister_id: String,
+    pub post_id: u64,
+    pub video_id: String,
+    pub nsfw_probability: f32,
+}
+
+impl Eq for PostItemV2 {}
+
+impl PartialEq for PostItemV2 {
+    fn eq(&self, other: &Self) -> bool {
+        self.publisher_user_id == other.publisher_user_id && self.post_id == other.post_id
+    }
+}
+
+impl Hash for PostItemV2 {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.publisher_user_id.hash(state);
+        self.post_id.hash(state);
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, ToSchema, Debug)]
+pub struct FeedResponseV2 {
+    pub posts: Vec<PostItemV2>,
 }
