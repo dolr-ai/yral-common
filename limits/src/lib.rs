a40f2781402bbaad5bf7e6301892e90215050e9b
Prakash <prakash@gobazzinga.io>
2025-07-08 09:30:12 +0000

    feat: update limits

--- limits/src/lib.rs
+++ limits/src/lib.rs
@@ -1,20 +1,22 @@
 use std::ops::Range;
 
 // Withdraw limits
-pub const MIN_WITHDRAWAL_PER_TXN: u64 = 200;
-pub const MAX_WITHDRAWAL_PER_TXN: u64 = 500;
+pub const MIN_WITHDRAWAL_PER_TXN: u64 = 50;
+pub const MAX_WITHDRAWAL_PER_TXN: u64 = 100;
 pub const MAX_WITHDRAWAL_PER_DAY: u64 = 10000;
 
 // Reward limit
 pub const NEW_USER_SIGNUP_REWARD: u64 = 1000;
 pub const REFERRAL_REWARD: u64 = 5;
-pub const SATS_AIRDROP_LIMIT_RANGE: Range<u64> = 50..100;
+pub const SATS_AIRDROP_LIMIT_RANGE: Range<u64> = 25..35;
 pub const AIRDROP_REWARD: u64 = 1000;
 pub const AIRDROP_REWARD_PER_DAY: u64 = 10000;
 
 // Coin state control
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum CoinState {
+    C1,
+    C5,
     C10,
     C20,
     C50,
@@ -22,7 +24,7 @@ pub enum CoinState {
     C200,
 }
 
-pub const BET_COIN_ENABLED_STATES: [CoinState; 2] = [CoinState::C10, CoinState::C20];
+pub const BET_COIN_ENABLED_STATES: [CoinState; 3] = [CoinState::C1, CoinState::C5, CoinState::C10];
 pub const DEFAULT_BET_COIN_STATE: CoinState = CoinState::C10;
-pub const MAX_BET_AMOUNT: usize = 20;
+pub const MAX_BET_AMOUNT: usize = 10;
 pub const TEST_VAR_REMOVE_THIS: usize = 0;
