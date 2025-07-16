85bd6107f42c81aefd3e8d8dddb8afb4c8b6d916
Komal <157946119+komal-sai-yral@users.noreply.github.com>
2025-07-16 12:55:51 +0530

    Update post.rs

--- types/src/post.rs
+++ types/src/post.rs
@@ -20,3 +20,24 @@ pub struct FeedRequest {
 pub struct FeedResponse {
     pub posts: Vec<PostItem>,
 }
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct PostItemV2 {
+    pub publisher_user_id: Principal,
+    pub canister_id: Principal,
+    pub post_id: u64,
+    pub video_id: String,
+    pub is_nsfw: f32,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct FeedRequestV2 {
+    pub user_id: Principal,
+    pub filter_results: Vec<PostItemV2>,
+    pub num_results: u32,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct FeedResponseV2 {
+    pub posts: Vec<PostItemV2>,
+}
