da9b702beffac3e7f9d0eef4b157d25a9df37033
joel-medicala-yral <joel@gobazzinga.io>
2026-02-06 14:46:10 +0530

    feat: image support + deprecate luma (#158)
    * feat: add image-to-video support for Wan 2.5 and Wan 2.5 Fast models
    
    * feat: add image support to SpeechToVideoModel
    
    * feat: remove LumaLabs video generation model
    
    * Revert "feat: remove LumaLabs video generation model"
    
    This reverts commit 3b40eed2895a1dfb9615c9096393ab69928623a2.
    
    * feat: hide LumaLabs from prod providers list

--- videogen-common/src/adapter_registry.rs
+++ videogen-common/src/adapter_registry.rs
@@ -51,7 +51,6 @@ impl AdapterRegistry {
         let providers = vec![
             Wan25FastModel::get_provider_info(), // Default
             Wan25Model::get_provider_info(),
-            LumaLabsModel::get_provider_info(),
         ];
 
         ProvidersResponse {
